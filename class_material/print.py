69e80131e43b43348891def4dfa1a3127090ef3c
harrissusanto91 <119999193+harrissusanto91@users.noreply.github.com>
2025-01-16 04:13:06 +0000

    Adding print.py

---
+++ class_material/print.py
@@ -0,0 +1,11 @@
+# Print function
+print("Hello, World!")
+
+# Input function
+name = input("Enter your name : ")
+age = int(input("Enter your age: "))
+
+
+# Combine input and print
+print(f"Hello, {name}! You are {age} years old.")
+print(f"Next year you will be {age + 1} years old.")
