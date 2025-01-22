c007297711bdbe8c61931c74ba6f3c299c220e8e
harrissusanto91 <119999193+harrissusanto91@users.noreply.github.com>
2025-01-22 08:42:20 +0000

    submit

---
+++ print/i_whish.py
@@ -0,0 +1,7 @@
+#Asking user name & height
+name = input("please state your name: ")
+height = float(input("how tall are you: "))
+
+#Returning the custom message
+print(f"So your name is: {name}")
+print (f"yes I am {height}cm tall, and I wish i am {height+10}cm tall")#the ideal height is +10
